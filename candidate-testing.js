daa89d3771e6474953310051cd22ca1592323e92
Kelsie Rushton <>
2021-06-04 01:52:06 +0000

    Made the Changes requested for Part 1 of Assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,12 +15,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("What is your name?");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
@@ -29,20 +30,23 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
+   if (candidateAnswer === correctAnswer) {
+    console.log("you have answered the question correctly");
+  } else {
+    console.log("you have answered the question incorrectly");
+  }
   let grade;
-  
-
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Welcome, " + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-
+ 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
