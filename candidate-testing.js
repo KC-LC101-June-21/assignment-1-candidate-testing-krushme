6598bb8016535a9d0b006c8e99097d4b2b896d9c
Kelsie Rushton <77713384+krushme@users.noreply.github.com>
2021-06-11 02:01:21 +0000

    made updates required for assignment 1.2

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,9 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+const correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -21,19 +21,30 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  //candidateAnswer = input.question(question); //
+ //for (let i = 0; i < questions.length; i++) {
+  //  input.question(questions[i])
+ // } 
 
-}
+  let i = 0;
+  while (i < questions.length) {
+    candidateAnswers[i] = input.question(questions[i]);
+    i++;
+  }
+
+
+  }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-   if (candidateAnswer === correctAnswer) {
-    console.log("you have answered the question correctly");
-  } else {
-    console.log("you have answered the question incorrectly");
+  //  if (candidateAnswer === correctAnswer) {
+  //   console.log("you have answered the question correctly");
+  // } else {
+  //   console.log("you have answered the question incorrectly");
+  // }
+  for (let i = 0; i < questions.length; i++) { 
+    console.log(`for the quiz you anwered ${candidateAnswers[i]} and the correct answer was ${correctAnswers[i]}.`);
   }
   let grade;
   return grade;
